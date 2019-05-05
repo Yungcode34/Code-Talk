8eec9e3920e5e691dc1c3f8c5402976c63c066f0
unknown <andrescardenas34@outlook.com>
2019-05-05 17:59:22 -0400

    starting

---
+++ src/components/Auth/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class Register extends React.Component {
+    render(){
+        return(
+            <div>Regsiter</div>
+        )
+    }
+}
+
+export default  Register;
