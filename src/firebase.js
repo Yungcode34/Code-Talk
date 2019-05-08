c7e6f05d941befca672bcca4c3921e55a587e390
butterrzz <38452206+butterrzz@users.noreply.github.com>
2019-05-07 20:31:33 -0400

    starting firebase server

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Your web app's Firebase configuration
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/storage";
+
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB9vAn8BL8nxgmPkP63fy-Azy8wwZrZ_fs",
+    authDomain: "react-code-talk.firebaseapp.com",
+    databaseURL: "https://react-code-talk.firebaseio.com",
+    projectId: "react-code-talk",
+    storageBucket: "react-code-talk.appspot.com",
+    messagingSenderId: "799283166689",
+    appId: "1:799283166689:web:ce9161e97925bc4c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
