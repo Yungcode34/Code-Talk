a5ae2f7dd85a33c4ca13b4dbff584744e308c1eb
unknown <andrescardenas34@outlook.com>
2019-05-11 03:08:28 -0400

    channel name updated, number of users displayed

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -3,16 +3,20 @@ import { Header, Segment, Input, Icon } from 'semantic-ui-react';
 
 class MessagesHeader extends React.Component{
     render(){
+
+const {channelName, numUniqueUsers} = this.props;
+
         return (
             <Segment clearing>
             {/*Channel title */}
             <Header fluid="true" as="h2" floated="left" style={{marginBottom:0}}>
                <span>
+                   {channelName}
                 Channel
             <Icon name={"star outline"} color="black"/>
                 </span>
                 <Header.Subheader>
-                    3 Users
+                    {numUniqueUsers}
                 </Header.Subheader>
             </Header>
 
