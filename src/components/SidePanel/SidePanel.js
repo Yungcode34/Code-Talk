6dc6bbf86d802b0f3f2ab95359142ce8d308691a
unknown <andrescardenas34@outlook.com>
2019-05-16 22:32:25 -0400

    starred channels will apear on users control panel

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -18,7 +18,7 @@ class SidePanel extends React.Component{
             
             >
                 <UserPanel currentUser={currentUser}/>
-                <Starred />
+                <Starred currentUser={currentUser} />
                 <Channels currentUser={currentUser} />
                 <DirectMessages currentUser={currentUser}/>
             </Menu>
--- src/components/SidePanel/Starred.js
+++ src/components/SidePanel/Starred.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import {Menu, Icon} from 'semantic-ui-react';
+import firebase from '../../firebase';
 import { connect} from 'react-redux';
 import { setCurrentChannel, setPrivateChannel} from '../../actions';
 
 class Starred extends React.Component {
 
     state = {
+        user: this.props.currentUser,
+        userRef: firebase.database().ref('users'),
         activeChannel:'',
         starrChannels: []
+    }
+
+    componentDidMount(){
+        if(this.state.user){
+            this.addListeners(this.state.user.uid);
+        }
+    }
+
+    addListeners = userId =>{
+        this.state.userRef
+        .child(userId)
+        .child('starred')
+        .on('child_added', snap =>{
+            const starrChannel = {id: snap.key, ...snap.val()};
+            this.setState({
+                starrChannels: [...this.state.starrChannels, starrChannel]
+            });
+        });
+        this.state.userRef
+        .child(userId)
+        .child('starred')
+        .on('child_removed', snap=>{
+            const channelToRemove ={id: snap.key, ...snap.val()}
+            const filteredChannels = this.state.starrChannels.filter(channel =>{
+                return channel.id !== channelToRemove.id;
+            })
+            this.setState({starrChannels: filteredChannels});
+        })
+
     }
 
     changeChannel = channel => {
